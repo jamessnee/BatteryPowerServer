cc49f0af685936a1f04c300756c6cb9adfd429e6
James Snee <james.snee@gmail.com>
2013-07-16 12:35:32 +0100

    Some logging code and a packet parsing method

--- src/uk/ac/cam/cl/dtg/UDPReceiver.java
+++ src/uk/ac/cam/cl/dtg/UDPReceiver.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class UDPReceiver implements Runnable {
 
@@ -15,6 +17,7 @@ public class UDPReceiver implements Runnable {
 
     @Override
     public void run() {
+        Logger.logDebug("UDPReceviver Started");
         DatagramSocket socket = null;
         try {
             socket = new DatagramSocket(port);
@@ -22,14 +25,33 @@ public class UDPReceiver implements Runnable {
             e.printStackTrace();
         }
 
-        DatagramPacket packet = new DatagramPacket(new byte[3], 3);
+        Logger.logDebug("Socket opened");
+        DatagramPacket packet = new DatagramPacket(new byte[2], 2);
 
+        Logger.logDebug("Waiting for packets...");
         while(true) {
             try {
                 socket.receive(packet);
+                parsePacket(packet);
             } catch(IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void parsePacket(DatagramPacket packet) {
+        if(Constants.DEBUG) {
+            Logger.logDebug("Packet received. Length: "+packet.getData());
+
+            Logger.logDebug("Part 1:"+packet.getData()[0]);
+            Logger.logDebug("Part 2:"+packet.getData()[1]);
+//            ByteBuffer bb = ByteBuffer.allocate(2);
+//            bb.order(ByteOrder.LITTLE_ENDIAN);
+//            bb.put(packet.getData()[0]);
+//            bb.put(packet.getData()[1]);
+//            short battPercentage = bb.getShort();
+
+//            Logger.logDebug("Reported Percentage: "+battPercentage);
         }
     }
 }
